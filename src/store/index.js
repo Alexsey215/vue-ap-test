def44a16ca65545a0925e9a12bd70602ef413748
Alexsey215 <orlov200015@gmail.com>
2022-07-23 19:14:55 +0700

    add fetch users

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [],
+    posts: []
   },
   getters: {
+    getUsers(state) {
+      return state.users
+    }
   },
   mutations: {
+    setUsers(state, payload) {
+      state.users = payload;
+    }
   },
   actions: {
+    async fetchUsers(context) {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
+        console.log(users)
+        context.commit('setUsers', users);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
   },
-  modules: {
-  }
 })
